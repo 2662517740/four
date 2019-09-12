f5763c6492eb29e02f44d5cd6858575e7d8c1b15
wuyangfang <2662517740@qq.com>
2019-09-12 17:58:33 +0800

    启动类

---
+++ src/main/java/com/h5/RunTest.java
@@ -0,0 +1,11 @@
+package com.h5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTest {
+    public void main(String args[]){
+        SpringApplication.run(RunTest.class ,args);
+    }
+}
